0e218f16d50c4e83433242277601ed1373f85e7e
Juniardi <juniardi.devops@gmail.com>
2022-09-19 11:06:35 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize  } from "sequelize";
 
-const db = new Sequelize('piccante', 'root', '', {
+const db = new Sequelize('piccante', 'root', '#K0p1d03l03', {
     host: 'localhost',
     dialect: 'mysql'
 })
